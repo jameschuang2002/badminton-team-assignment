74441d67290238703b46de7b0a13f508d3b1a5be
James Chuang <jameschuang2002@gmail.com>
2025-04-01 11:56:48 -0400

    add present players feature and support add and remove players for table

--- static/main.js
+++ static/main.js
@@ -24,7 +24,38 @@ function getPlayers() {
       })
 }
 
-var all_players;
+function getPresentPlayersNames(){
+    let players_dropdown = document.querySelector("div#players_dropdown_div");
+    let dropdown_menu = players_dropdown.querySelector("ul");
+    let dropdown_players = players_dropdown.querySelectorAll("a")
+    let present_players = []
+    for(let item of dropdown_players){
+        let checkbox = item.querySelector("input");
+        if(checkbox.checked){
+            present_players.push(checkbox.value);
+        }
+    }
+    return present_players
+}
+
+function addRow(tbody, name1, name2){
+    let row = document.createElement("tr");
+    tbody.appendChild(row);
+
+    let label = document.createElement("th");
+    label.scope = "row";
+    row.appendChild(label)
+
+    let member1 = document.createElement("td") 
+    let member2 = document.createElement("td") 
+    
+    member1.innerHTML = name1;
+    member2.innerHTML = name2;
+    row.appendChild(member1);
+    row.appendChild(member2);
+
+    tbody.appendChild(row)
+}
 
 window.addEventListener("load", function(){
     let h1 = document.querySelector("h1");
@@ -52,9 +83,12 @@ window.addEventListener("load", function(){
             checkbox.class = "form-check-input me-2";
             checkbox.value = player.name;
             checkbox.checked="yes";
+
+            let checkbox_label = document.createElement("label");
+            checkbox_label.appendChild(checkbox)
+            checkbox_label.appendChild(document.createTextNode(" " + player.name))
             
-            dropdown_item.appendChild(checkbox);
-            dropdown_item.appendChild(document.createTextNode(" " + player.name))
+            dropdown_item.appendChild(checkbox_label);
         }
 
         // setup initial teams and table structure
@@ -64,39 +98,31 @@ window.addEventListener("load", function(){
         let table_body = team_table.querySelector("tbody")
         
         for(let i = 0; i < all_players.length / 2; i++){
-            let row = document.createElement("tr");
-            team_table.appendChild(row);
-
-            let label = document.createElement("th");
-            label.scope = "row";
-            row.appendChild(label)
-
-            let member1 = document.createElement("td") 
-            let member2 = document.createElement("td") 
-            
-            member1.innerHTML = all_players[i*2].name;
-            member2.innerHTML = all_players[i*2+1].name;
-            row.appendChild(member1);
-            row.appendChild(member2);
-
-            table_body.appendChild(row)
+            addRow(table_body, players[i*2].name, players[i*2+1].name);
         }
 
-        all_players = players; // TODO: remove this variable after read from checkbox function is done
     }).catch((error) => {
         console.log("error fetching players", error)
     })
 })
 
 document.getElementById("randomizeButton").onclick = function(){
-    shuffleArray(all_players)
+    const present_players_names = getPresentPlayersNames();
+    shuffleArray(present_players_names)
     /* TODO: only compute on present players, remove extra rows */
     let team_table = document.querySelector("table#team_table");
     let table_body = document.querySelector("tbody");
     let rows = table_body.querySelectorAll("tr");
     for(let i = 0; i < rows.length; i++){
-        team_members = rows[i].querySelectorAll("td");
-        team_members[0].innerHTML = all_players[i*2].name;
-        team_members[1].innerHTML = all_players[i*2+1].name;
+        if(i * 2 >= present_players_names.length){
+            table_body.removeChild(rows[i])
+        } else{
+            team_members = rows[i].querySelectorAll("td");
+            team_members[0].innerHTML = present_players_names[i*2];
+            team_members[1].innerHTML = present_players_names[i*2+1];
+        }
+    }
+    for(let i = 0; i < (present_players_names.length - rows.length * 2)/2; i++){
+        addRow(table_body, present_players_names[(rows.length + i) * 2], present_players_names[(rows.length + i) * 2 + 1])
     }
 }
